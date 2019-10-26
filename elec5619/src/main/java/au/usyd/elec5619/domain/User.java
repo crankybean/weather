84827e743aa70fa25bb0e896b033341bf8400eb4
crankybean <50895059+crankybean@users.noreply.github.com>
2019-10-27 04:58:59 +1100

    add user

---
+++ elec5619/src/main/java/au/usyd/elec5619/domain/User.java
@@ -0,0 +1,78 @@
+package au.usyd.elec5619.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User implements Serializable{
+	
+	@Id
+	@GeneratedValue
+	@Column(name="Id",nullable=false, unique=true)
+	private int id;
+	
+	@Column(name="UserName",nullable=false)
+	private String userName;
+	
+	@Column(name="Password", nullable=false)
+	private String password;
+	
+	@Column(name="Email", nullable=false)
+	private String email;
+	
+	@Column(name="ProfilePic", nullable=false)
+	private String profilePic;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public String getProfilePic() {
+		return profilePic;
+	}
+
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+//	public String toString() {
+//		StringBuffer buffer = new StringBuffer();
+//		buffer.append("Description: " + description + ";");
+//		buffer.append("Price: " + price);
+//		return buffer.toString();
+//	}
+}
