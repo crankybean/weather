a98273d86351593cc02c84b86ffcc881b931b245
xianzhe yang <yangxianzhe123@hotmail.com>
2019-10-29 02:31:57 +1100

    after hour

--- elec5619/src/main/java/au/usyd/elec5619/web/InvertoryController.java
+++ elec5619/src/main/java/au/usyd/elec5619/web/InvertoryController.java
@@ -1,28 +1,25 @@
 package au.usyd.elec5619.web;
-import java.util.Date;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponse;
-
+import au.usyd.elec5619.service.ProductManager;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-import au.usyd.elec5619.service.ProductManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InvertoryController implements Controller {
 	
-	protected final Log logger = LogFactory.getLog(getClass());
+	private final Log logger = LogFactory.getLog(getClass());
 	
 	private ProductManager productManager;
+	@Override
 	public ModelAndView handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+									  HttpServletResponse response) throws Exception {
 		String now = (new Date()).toString();
 		logger.info("Returning hello view with " + now);
 		
